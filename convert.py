12817abf69d107a2608ef8324907b286a914046b
Serafim Nenarokov <serafim221@gmail.com>
2021-11-04 16:33:16 +0100

    Init repository

---
+++ convert.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# @Date    : 2021-10-27
+# @Author  : Serafim Nenarokov (serafim.nenarokov@gmail.com)
+
+from argparse import ArgumentParser
+from pprint import pprint
+
+import os
+import openpyxl
+from pathlib import Path
+from jinja2 import Template
+
+from lib.record import Record
+
+
+def read_args():
+  parser = ArgumentParser(description = 'Gets GC statistics from fasta file and annotation GFF results')
+  parser.add_argument('-i', '--input', required = True, help = 'path to XLSX file')
+  parser.add_argument('-o', '--output', required = True, help = 'path to output folder for XML files')
+  return parser.parse_args()
+
+def check_output_folder(val):
+  if not os.path.isdir(val):
+    print("Output path is not a directory or doesn't exist.")
+    exit(-1)
+
+def get_entries(sheet):
+  result = []
+
+  col_names = []
+  for column in sheet.iter_cols(1, sheet.max_column):
+    col_names.append(column[0].value)
+
+  for row in sheet.iter_rows(min_row=2):
+    obj = {}
+
+    for i, cell in enumerate(row):
+      val = cell.value
+
+      if val:
+        obj[col_names[i]] = val
+
+    if len(obj) > 0:
+      result.append(obj)
+
+  return result
+
+
+def main():
+  args = read_args()
+  check_output_folder(args.output)
+
+  xlsx_path = Path(args.input)
+  obj = openpyxl.load_workbook(xlsx_path)
+
+  data_sheet = obj.active
+
+  records = []
+
+  for entry in get_entries(data_sheet):
+    record = Record(entry)
+    records.append(record)
+
+    record.validate()
+    if not record.is_valid():
+      record.print_errors()
+
+  ## working with template
+  with open('template.xml') as f:
+    template = Template(f.read())
+
+  for record in records:
+    text = template.render(record=record)
+    out_path = os.path.join(args.output, f'{record.invoice_number}.xml')
+    
+    with open(out_path, 'w') as out_f:
+      out_f.write(text)
+
+
+if __name__ == '__main__':
+  main()
---
+++ lib/record.py
@@ -0,0 +1,93 @@
+from datetime import datetime
+
+class Record():
+  @staticmethod
+  def parse_date(date):
+    if type(date) != datetime:
+      return datetime.fromisoformat(date)
+
+    return date
+
+
+  @staticmethod
+  def is_value_empty(val):
+    if type(val) == str and val.strip() == '':
+      return True
+
+    return val == None or val == False
+
+
+  def __init__(self, xls_dict):
+    self.errors = []
+
+    self.xls_dict = xls_dict
+    self.fields = ("personal_number",
+                   "first_name",
+                   "last_name",
+                   "birth_date",
+                   "sex",
+                   "invoice_number",
+                   "test_date",
+                   "exam_date",
+                   "result_date",
+                   "pango",
+                   "variant")
+
+    self.personal_number = xls_dict['Rodné číslo']
+    self.first_name = xls_dict['Jméno']
+    self.last_name = xls_dict['Příjmení']
+    self.birth_date = Record.parse_date(xls_dict['Datum narození'])
+    self.sex = xls_dict['Pohlaví'].upper()
+
+    self.invoice_number = xls_dict['Číslo žádanky']
+
+    self.test_date = Record.parse_date(xls_dict['Datum odběru'])
+    self.exam_date = Record.parse_date(xls_dict['Datum vyšetření'])
+    self.result_date = Record.parse_date(xls_dict['Datum výsledku'])
+
+    self.pango = xls_dict['Pango linie']
+    self.variant = xls_dict['Varianta dle číselníku']
+
+    # removing space symbols around 
+    for fld in self.fields:
+      val = getattr(self, fld)
+
+      if type(val) == str:
+        setattr(self, fld, val.strip())
+
+  def __str__(self):
+    data = [f'{k}: "{v}"' for k, v in self.xls_dict.items()]
+    return f'[{ ", ".join(data) }]'
+
+
+  def validate(self):
+    for fld in self.fields:
+      val = getattr(self, fld)
+
+      if Record.is_value_empty(val):
+        self.errors.append(f'the field {fld} is empty')
+
+    if self.sex not in ('M', 'Ž'):
+      self.errors.append(f'the field "sex" can only have values M or Ž')
+
+
+  def is_valid(self):
+    return len(self.errors) == 0
+
+
+  def print_errors(self):
+    if self.is_valid():
+      return False
+
+    errors = ', '.join(self.errors)
+    errors += '.'
+
+    print(f"The record {str(self)} has problems: {errors}")
+
+  
+
+
+
+
+
+
